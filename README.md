705996f2beaab7a76429fc11a95449829d0bb43c
dwairi <qaisdw96@hotmail.com>
2023-05-02 11:43:20 +0000

    First_Lab

---
+++ README.md
@@ -0,0 +1,29 @@
+
+# LAB - Class 01
+## Project: Snakes Shop
+## Author: Qais Dwairi
+## Links and Resources :-
+    
+ [Python Lab Submission Instructions](https://codefellows.github.io/code-401-python-guide/reference/submission-instructions/labs/)
+
+
+
+### How to initialize/run your application (where applicable)
+
+    > python3 snakes_cafe.py
+
+### How to use your library (where applicable) 
+### Tests
+
+    - How do you run tests?
+         1. first you need to install pytest or pytest-watch.
+         **Run these command**
+             1.pip install pytest
+             2.pip freeze > requirements.txt
+             3.touch tests/__init__.py
+             4.touch tests/test_example.py
+
+    - Any tests of note?
+       *no*
+    - Describe any tests that you did not complete, skipped, etc
+       i didn't skip any tests 
---
+++ snakes_cafe.py
@@ -0,0 +1,41 @@
+menu = ['Cookies', 'Salmon', 'Steak', 'Meat Tornado', 'Spring Rolls']
+# list comprehension which creates list of tuples
+order_counts = [(item, 0) for item in menu]
+
+
+def intro():
+    print('''
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+''')
+
+def Snaks():
+    intro()
+    user_input = ''
+    while user_input.lower() != 'quit':
+        user_input = input('> ').title().strip()
+        # cheack if the input NOT empty
+        if user_input != '':
+            if user_input in menu:
+                index = menu.index(user_input)
+                item, count = order_counts[index]
+                order_counts[index] = (item, count + 1)
+                print(f'** {count + 1} order of {item} have been added to your meal **')
+            else:
+                print('Sorry, we don\'t have this item!')
+
+    end_application()
+
+
+def end_application():
+    print('Thanks for using the Snakes Cafe application!')
+
+
+if __name__ == '__main__':
+    Snaks()
+
+
